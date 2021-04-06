89fe7f514b1aef4a75ab7c2f964641533548ee70
Havishka <havishkahalangoda@gmail.com>
2021-04-06 21:14:56 +0530

    Adding hooks

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
+    <?php wp_head();?>
 </head>
-<body>
-    <head>Head</head>
+<body> <?php body_class(); ?>
+<?php wp_body_open(); ?>
+    <header>Header</header>
